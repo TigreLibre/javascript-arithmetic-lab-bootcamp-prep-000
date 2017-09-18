f3cf81e15d675a6be6c747a3380f96ca3ed453da
TigreLibre <rachelposey4@gmail.com>
2017-09-18 01:30:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ function dec(a) {
 function makeInt(a) {
   return (a)
 }
+
+function preserveDecimal(2.222) {
+  return (2.222)
+}
+
+function isNan(preserveDecimal('sldkjflksjf') {
+  return (true)
+}
+
+
